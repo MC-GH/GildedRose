0303ca7d037a5a9f13c5df142ef67dabb3e36983
Michaël <cornelismichael@hotmail.com>
2023-12-13 21:43:12 +0100

    Start reworking base logic with switch statement. Small change in decreaseQuality method, some changes in updateBackstagePassQuality method. (not yet all tests passing)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,52 +12,65 @@ class GildedRose {
 
             String name = item.name;
 
-            if(name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-
-            }
-
-
-
-            if (!name.equals("Aged Brie")
-                    && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality(item);
-                }
-            } else {
-                increaseQuality(item);
-                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                            increaseQuality(item);
-                    }
-
-                    if (item.sellIn < 6) {
-                            increaseQuality(item);
-                    }
-                }
-            }
-
+            //Decrease sell in date by 1 for all items except Sulfuras
             if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                 decreaseSellIn(item);
+
+                switch (name) {
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        updateBackstagePassQuality(item);
+                        break;
+                    case "Aged Brie":
+                        increaseQuality(item);
+                        break;
+                    default:
+                        decreaseQuality(item);
+                }
+
             }
 
-            if (item.sellIn < 0) {
-                if (!name.equals("Aged Brie")) {
-                    if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decreaseQuality(item);
-                            }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                        increaseQuality(item);
-                }
-            }
+
+//            if (!name.equals("Aged Brie")
+//                    && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+//                        decreaseQuality(item);
+//                }
+//            } else {
+//                increaseQuality(item);
+//                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                    if (item.sellIn < 11) {
+//                            increaseQuality(item);
+//                    }
+//
+//                    if (item.sellIn < 6) {
+//                            increaseQuality(item);
+//                    }
+//                }
+//            }
+//
+//            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+//                decreaseSellIn(item);
+//            }
+//
+//            if (item.sellIn < 0) {
+//                if (!name.equals("Aged Brie")) {
+//                    if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+//                            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+//                                decreaseQuality(item);
+//                            }
+//                    } else {
+//                        item.quality = item.quality - item.quality;
+//                    }
+//                } else {
+//                        increaseQuality(item);
+//                }
+//            }
         }
     }
 
     public void decreaseQuality(Item item) {
         if (item.quality > 0) item.quality--;
+        if (item.quality > 0 && item.sellIn <= 0) item.quality--;
     }
 
     public void increaseQuality(Item item) {
@@ -65,7 +78,7 @@ class GildedRose {
     }
 
     public void increaseQuality(Item item, int frequency) {
-        for(int i = 0; i < frequency; i++) {
+        for (int i = 0; i < frequency; i++) {
             increaseQuality(item);
         }
     }
@@ -76,14 +89,15 @@ class GildedRose {
 
     public void updateBackstagePassQuality(Item item) {
         int sellIn = item.sellIn;
-        if(sellIn > 10) {
-            increaseQuality(item);
-        } else if (sellIn <= 10) {
-            increaseQuality(item, 2);
-        } else if (sellIn < 6) {
-            increaseQuality(item,3);
-        } else {
+
+        if (sellIn < 0) {
             item.quality = 0;
+        } else if (sellIn < 6) {
+            increaseQuality(item, 3);
+        } else if (sellIn < 11) {
+            increaseQuality(item, 2);
+        } else {
+            increaseQuality(item);
         }
     }
 
