a061d68ec36483fbffd97c3aeb5202afb9020a4b
Michaël <cornelismichael@hotmail.com>
2024-01-15 10:18:16 +0100

    clean up

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -32,43 +32,6 @@ class GildedRose {
 
             decreaseSellIn(item);
         }
-
-
-//            if (!name.equals("Aged Brie")
-//                    && !name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-//                        decreaseQuality(item);
-//                }
-//            } else {
-//                increaseQuality(item);
-//                if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                    if (item.sellIn < 11) {
-//                            increaseQuality(item);
-//                    }
-//
-//                    if (item.sellIn < 6) {
-//                            increaseQuality(item);
-//                    }
-//                }
-//            }
-//
-//            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-//                decreaseSellIn(item);
-//            }
-//
-//            if (item.sellIn < 0) {
-//                if (!name.equals("Aged Brie")) {
-//                    if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-//                                decreaseQuality(item);
-//                            }
-//                    } else {
-//                        item.quality = item.quality - item.quality;
-//                    }
-//                } else {
-//                        increaseQuality(item);
-//                }
-//            }
     }
 
 
